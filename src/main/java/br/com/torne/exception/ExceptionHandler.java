aa9e501d8892684447fd5ddc18b88f6f15088770
Franklin Soares <franklindeos@gmail.com>
2020-10-16 21:51:25 -0300

    Exceptions

--- src/main/java/br/com/torne/exception/ExceptionHandler.java
+++ src/main/java/br/com/torne/exception/ExceptionHandler.java
@@ -2,11 +2,18 @@ package br.com.torne.exception;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +21,13 @@ import br.com.torne.exception.dto.ErroDto;
 
 @RestControllerAdvice
 public class ExceptionHandler {
+	
+	private MessageSource messageSource;
+
+	@Autowired
+	public ExceptionHandler(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@ResponseStatus(code = HttpStatus.NOT_FOUND)
 	@org.springframework.web.bind.annotation.ExceptionHandler(value = {EntityNotFoundException.class, EmptyResultDataAccessException.class})
@@ -35,7 +49,28 @@ public class ExceptionHandler {
 		return listDto;
 	}
 	
+	@org.springframework.web.bind.annotation.ExceptionHandler(RestException.class)
+    public ResponseEntity<RestMessage> handleIllegalArgument(RestException ex, Locale locale) {
+        String errorMessage = messageSource.getMessage(ex.getMessage(), ex.getArgs(), locale);
+        return new ResponseEntity<>(new RestMessage(errorMessage), HttpStatus.BAD_REQUEST);
+    }
 	
+	@org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+	    public ResponseEntity<RestMessage> handleArgumentNotValidException(MethodArgumentNotValidException ex, Locale locale) {
+	        BindingResult result = ex.getBindingResult();
+	        List<String> errorMessages = result.getAllErrors()
+	                .stream()
+	                .map(objectError -> messageSource.getMessage(objectError, locale))
+	                .collect(Collectors.toList());
+	        return new ResponseEntity<>(new RestMessage(errorMessages), HttpStatus.BAD_REQUEST);
+	    }
+
+	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+	    public ResponseEntity<RestMessage> handleExceptions(Exception ex, Locale locale) {
+	        String errorMessage = messageSource.getMessage(UNEXPECTED_ERROR, null, locale);
+	        ex.printStackTrace();
+	        return new ResponseEntity<>(new RestMessage(errorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
 	
 
 }
--- src/main/java/br/com/torne/utils/ValidationUtils.java
+++ src/main/java/br/com/torne/utils/ValidationUtils.java
@@ -1,5 +1,53 @@
 package br.com.torne.utils;
 
-public class ValidationUtils {
+import java.util.Collection;
+import java.util.Map;
 
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+
+import br.com.torne.exception.RestException;
+
+public final class Check {
+    public static void isNull(Object object, String message, Object... args) {
+        if (object != null) {
+            throw new RestException(message, args);
+        }
+    }
+
+    public static void isTrue(boolean expression, String message, Object... args) {
+        if (!expression) {
+            throw new RestException(message, args);
+        }
+    }
+
+    public static void notNull(Object object, String message, Object... args) {
+        if (object == null) {
+            throw new RestException(message, args);
+        }
+    }
+
+    public static void notEmpty(Object[] array, String message, Object... args) {
+        if (ObjectUtils.isEmpty(array)) {
+            throw new RestException(message, args);
+        }
+    }
+
+    public static void notEmpty(Collection<?> collection, String message, Object... args) {
+        if (CollectionUtils.isEmpty(collection)) {
+            throw new RestException(message, args);
+        }
+    }
+
+    public static void notEmpty(Map<?, ?> map, String message, Object... args) {
+        if (CollectionUtils.isEmpty(map)) {
+            throw new RestException(message, args);
+        }
+    }
+
+    public static void notEmpty(String text, String message, Object... args) {
+        if (text == null || "".equals(text.trim())) {
+            throw new RestException(message, args);
+        }
+    }
 }
